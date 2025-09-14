48670d690cd77e40c4b552b046bfc9ff7823442e
Dustin Coker <dustincoker@icloud.com>
2025-09-14 02:53:57 -0400

    Update addCssLoaderPlugin.php

--- custom/Extension/application/Ext/JSGroupings/addCssLoaderPlugin.php
+++ custom/Extension/application/Ext/JSGroupings/addCssLoaderPlugin.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * Copyright 2016 SugarCRM Inc.  Licensed by SugarCRM under the Apache 2.0 license.
- */
 
 //Loop through the groupings to find grouping file you want to append to
 foreach ($js_groupings as $key => $groupings)
